b6e43ff3e68ff11886b89176dd4e977ffab82658
Luan Lima <luanlima121212@gmail.com>
2020-06-17 21:01:07 -0300

    questão 10 atualizada

--- projetox/includes/quest10part2.php
+++ projetox/includes/quest10part2.php
@@ -4,7 +4,7 @@
             $num2 = $_POST['dois'];
 
               function calculo($num1=1, $num2=1){
-                return ($num1/$num2);
+                return ($num2/$num1);
               }
             
               echo "<br><br><br>O consumo médio de combustivel é ".number_format(calculo($num1, $num2),2)."
